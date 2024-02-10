fdcd646264de1fca53fba7579d0fff3c8919e4df
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-02-10 14:21:31 +0500

    feat: добавить главную страницу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,24 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import backgroundMain from "../../public/background-main.jpg"
+import "./page.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main" aria-label="Автосервис БГП авто, главная страница">
+      <Image className="main__background" src={backgroundMain} alt={"Фоновое изображение с ремонтом авто"} />
+      <div className="main__container">
+        <div className="main__title-container">
+          <div className="main__title-icon main__title-icon_red" />
+          <h2 className="main__title-text">Ремонт и обслуживание<br />автомобилей VAG группы более 5 лет</h2>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="main__title-container">
+          <div className="main__title-icon main__title-icon_black" />
+          <h2 className="main__title-text">Использование официальной технической<br />документации и специнструмента</h2>
+        </div>
+        <div className="main__title-container">
+          <div className="main__title-icon main__title-icon_black" />
+          <h2 className="main__title-text">Обслуживание автомобилей других марок</h2>
+        </div>
       </div>
     </main>
   );
